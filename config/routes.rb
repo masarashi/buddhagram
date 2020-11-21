e6fb4b94b6b098618a35aa1b8c03946b2f1a69fe
Masao Igarashi <masarashi@gmail.com>
2020-11-21 14:38:22 +0900

    #11 タイムライン機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
   root 'posts#index'
+
+  # devise_scope :user do
+  #   root "users/sessions#new"
+  # end
 
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
     registrations: 'users/registrations'
   }
+
   resources :users, only: [:show] do
     member do
       get :following, :followers
     end
+    get '/timeline', to: 'users#timeline'
   end
+
+  # get '/:id/timeline', to: 'users#timeline'
 
   resources :posts do
     resources :comments, only: [:create]
