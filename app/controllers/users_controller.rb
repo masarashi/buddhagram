bf974a91bdf0b50bb8250b3dd316fe3fd561c398
Masao Igarashi <masarashi@gmail.com>
2020-12-13 12:09:50 +0900

    bulletアラート修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.all.order(created_at: :desc)
+    @posts = @user.posts.includes(:images_attachments).order(created_at: :desc)
     @feed_items = @user.feed
   end
 
