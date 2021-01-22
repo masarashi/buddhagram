ad12f34b2f006c750d300a2c89481fbb2fc9226a
Masao Igarashi <masarashi@gmail.com>
2021-01-22 23:52:48 +0900

    デモデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do |i|
-  User.find_by(email: "demouser#{i}@example.com").destroy
-end
+# 20.times do |i|
+#   User.find_by(email: "demouser#{i}@example.com").destroy
+# end
 
-20.times do |i|
-  User.create(email: "demouser#{i}@example.com",
-              name: Faker::Name.name,
-              profile: Faker::Lorem.paragraph,
-              password: "demopassword#{i}").image.attach(io: File.open("app/javascript/images/users/demouser#{i}.png"),
-                                                         filename: "demouser#{i}.png",
-                                                         content_type: 'image/png')
+# 20.times do |i|
+#   User.create(email: "demouser#{i}@example.com",
+#               name: Faker::Name.name,
+#               profile: Faker::Lorem.paragraph,
+#               password: "demopassword#{i}").image.attach(io: File.open("app/javascript/images/users/demouser#{i}.png"),
+#                                                          filename: "demouser#{i}.png",
+#                                                          content_type: 'image/png')
+# end
+
+# require 'csv'
+
+# CSV.foreach('db/seeds/statues.csv', headers: true) do |row|
+#   Statue.create(
+#     name: row['name'],
+#     ruby: row['ruby'],
+#     grade: row['grade'],
+#     content: row['content']
+#   )
+# end
+
+5.times do
+  images = Dir.glob('app/javascript/images/statues/*')
+  post = Post.new(user_id: User.all.sample.id, content: Faker::Lorem.sentence, spot_id: Spot.all.sample.id)
+  post.images.attach(io: File.open(images.sample), filename: images.sample)
+  post.save
 end
